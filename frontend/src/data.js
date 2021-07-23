a7dafa6265d017c963814b4a2d3402c932255061
Sam-Bahadur <pradipktimsina@gmail.com>
2021-07-23 11:51:52 +0545

    first page
    first page with product and rating component

---
+++ frontend/src/data.js
@@ -0,0 +1,78 @@
+const data = {
+    products:[
+        {
+            _id: '1',
+            name: 'Nike Slim Shirt',
+            category: 'Shirts',
+            image:'/images/p1.jpg',
+            price: 120,
+            brand: 'Nike',
+            brand: 'Nike',
+            rating: '4.5',
+            numReviews: 10,
+            description: 'high quality product'
+        },
+        {
+            _id: '2',
+            name: 'Umbro Slim Shirt',
+            category: 'Shirts',
+            image:'/images/p2.jpg',
+            price: 100,
+            brand: 'Umbro',
+            brand: 'Nike',
+            rating: '4',
+            numReviews: 11,
+            description: 'high quality product'
+        },
+        {
+            _id: '3',
+            name: 'Nike Slim Shirt',
+            category: 'Shirts',
+            image:'/images/p3.jpg',
+            price: 120,
+            brand: 120,
+            brand: 'Nike',
+            rating: '4.5',
+            numReviews: 10,
+            description: 'high quality product'
+        },
+        {
+            _id: '4',
+            name: 'Nike Slim Shirt',
+            category: 'Shirts',
+            image:'/images/p4.jpg',
+            price: 120,
+            brand: 120,
+            brand: 'Nike',
+            rating: '4.5',
+            numReviews: 10,
+            description: 'high quality product'
+        },
+        {
+            _id: '5',
+            name: 'Chinese Slim Shirt',
+            category: 'Shirts',
+            image:'/images/p5.jpg',
+            price: 20,
+            brand: 20,
+            brand: 'Chinese',
+            rating: '4.5',
+            numReviews: 10,
+            description: 'High Copy'
+        },
+        {
+            _id: '6',
+            name: 'Nike Slim Shirt',
+            category: 'Shirts',
+            image:'/images/p6.jpg',
+            price: 220,
+            brand: 220,
+            brand: 'Nike',
+            rating: '3.5',
+            numReviews: 20,
+            description: 'high quality product'
+        },
+    ]
+}
+
+export default data;
